f1f90b51935643ebff781c86b66045fe53e37f37
Sangdak <82779027+Sangdak@users.noreply.github.com>
2023-08-16 02:30:09 +1000

    Delete the debugging prints.

--- dialogflow_api.py
+++ dialogflow_api.py
@@ -30,7 +30,6 @@ def create_parser():
 def detect_intent_texts(session_id, text, project_id, language_code='ru-ru'):
     session_client = dialogflow.SessionsClient()
     session = session_client.session_path(project_id, session_id)
-    print(f'Session path: {session}', end='\n')
 
     text_input = dialogflow.TextInput(text=text, language_code=language_code)
     query_input = dialogflow.QueryInput(text=text_input)
@@ -39,14 +38,6 @@ def detect_intent_texts(session_id, text, project_id, language_code='ru-ru'):
         request={'session': session, 'query_input': query_input}
     )
 
-    print('=' * 20)
-    print(f'Query text: {response.query_result.query_text}')
-    print(
-        f'Detected intent: {response.query_result.intent.display_name} '
-        f'(confidence: {response.query_result.intent_detection_confidence})',
-        end='\n'
-    )
-    print(f'Fulfillment text: {response.query_result.fulfillment_text}', end='\n')
     return response.query_result.fulfillment_text, response.query_result.intent.is_fallback
 
 
